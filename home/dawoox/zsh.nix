2fa1bb4177ae11ce029cb8ee001bc38f9082a276
Antoine <me@antoinebellanger.fr>
2023-12-18 15:58:07 +0100

    [FEATURE] Improve zsh config

--- home/dawoox/zsh.nix
+++ home/dawoox/zsh.nix
@@ -24,6 +24,24 @@
           hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
         };
       }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.7.1";
+          hash = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
+        };
+      }
+      {
+        name = "zsh-autocomplete";
+        src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "eee8bbeb717e44dc6337a799ae60eda02d371b73";
+          hash = "sha256-2qkB8I3GXeg+mH8l12N6dnKtdnaxTeLf5lUHWxA2rNg=";
+        };
+      }
     ];
   };
 }
