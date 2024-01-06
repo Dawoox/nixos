ded2454501169fabf52c86725444c06d8357ad9c
Antoine <me@antoinebellanger.fr>
2024-01-07 00:19:08 +0100

    [ENHANCEMENT] Reduce zsh load time

--- home/dawoox/zsh.nix
+++ home/dawoox/zsh.nix
@@ -15,16 +15,8 @@
       alias epidock="sudo docker run -it --rm -v $(pwd):/home/project -w /home/project epitechcontent/epitest-docker:latest /bin/bash"
       alias waybar-reload="~/waybar_reload.sh"
     '';
+    enableAutosuggestions = true;
     plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.7.0";
-          hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-        };
-      }
       {
         name = "zsh-syntax-highlighting";
         src = pkgs.fetchFromGitHub {
@@ -34,15 +26,17 @@
           hash = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
         };
       }
-      {
-        name = "zsh-autocomplete";
-        src = pkgs.fetchFromGitHub {
-          owner = "marlonrichert";
-          repo = "zsh-autocomplete";
-          rev = "eee8bbeb717e44dc6337a799ae60eda02d371b73";
-          hash = "sha256-2qkB8I3GXeg+mH8l12N6dnKtdnaxTeLf5lUHWxA2rNg=";
-        };
-      }
+      # Need to be disable currently because its too
+      # cpu intensive, so it add 0.4s to zsh load
+      #{
+      #  name = "zsh-autocomplete";
+      #  src = pkgs.fetchFromGitHub {
+      #   owner = "marlonrichert";
+      #   repo = "zsh-autocomplete";
+      #   rev = "eee8bbeb717e44dc6337a799ae60eda02d371b73";
+      #   hash = "sha256-2qkB8I3GXeg+mH8l12N6dnKtdnaxTeLf5lUHWxA2rNg=";
+      # };
+      #}
     ];
   };
 }
