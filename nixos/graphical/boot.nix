c544e8a230adf0b65612236d8ccb920267f8d1b5
Antoine <me@antoinebellanger.fr>
2024-03-10 18:09:44 +0100

    [CHANGE] Systemd-boot -> Grub
    Better configuration capabilites but slower boot time, had to make a compromise here

--- nixos/graphical/boot.nix
+++ nixos/graphical/boot.nix
@@ -2,8 +2,17 @@
 {
   boot = {
     loader = {
-      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      timeout = 2;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = false;
+        extraConfig = ''
+          GRUB_CMDLINE_LINUX="mem_sleep_default=deep"
+        '';
+      };
     };
     plymouth.enable = true;
     kernelParams = [ "quiet" ];
